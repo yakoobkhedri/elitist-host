ed980676a080e05ea3a0f21d076be0e4faf71a8f
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-01 22:46:41 +0330

    commit message

---
+++ assets/js/scripts.js
@@ -0,0 +1,72 @@
+// swiper
+
+var services = new Swiper(".services", {
+  slidesPerView: 2,
+  spaceBetween: 98,
+  autoplay: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 6,
+    },
+    1400: {
+      slidesPerView: 9.5,
+    },
+  },
+});
+
+var customers = new Swiper(".customers", {
+  slidesPerView: 2,
+  spaceBetween: 98,
+  autoplay: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var blog = new Swiper(".blog", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+var testimonial = new Swiper(".testimonial", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  autoplay: true,
+});
+
+// datapicker
+
+jalaliDatepicker.startWatch();
+
+// aos
+
+AOS.init();
+
+// counter
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 1400
+});
