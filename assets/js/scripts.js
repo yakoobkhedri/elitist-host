fe04eec3aa02f0b4669e7e5fa4896cf937a25ca4
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-18 21:03:41 +0330

    commit message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -93,6 +93,20 @@ jalaliDatepicker.startWatch();
 
 AOS.init();
 
+// navigation
+
+const list = document.querySelectorAll('.list');
+
+function activeLink() {
+  list.forEach((item) =>
+    item.classList.remove('active'));
+  this.classList.add('active');
+}
+
+list.forEach((item) =>
+  item.addEventListener('mouseover', activeLink));
+
+
 // counter
 
 $('.counter').counterUp({
